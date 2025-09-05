b41d8d2dce4f2bd715d358e9c120c327c9a7b5ad
Thaiduis11 <thaidxhe187246@fpt.edu.vn>
2025-09-05 16:27:48 +0700

    feat: create gateway

--- laundry-microservice/src/main/java/com/laundry/Main.java
+++ laundry-microservice/src/main/java/com/laundry/Main.java
@@ -1,7 +1,12 @@
 package com.laundry;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaClient
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(Main.class, args);
     }
 }
